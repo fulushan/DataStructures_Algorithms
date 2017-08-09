e298a50e964f10eee84a07639e449f4e4a045f99
fulushan <lushan.fu@pp.plateno.cc>
2017-08-09 12:12:07 +0800

    [直接插入排序完成]

--- DataStructures-Algorithms/src/com/fulushan/dsa/sort/InsertSort.java
+++ DataStructures-Algorithms/src/com/fulushan/dsa/sort/InsertSort.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
  * <直接插入排序>
  * Author fulushan
  * Created  on 17/8/9.
+ * 时间复杂度 O(n2)
  */
 public class InsertSort {
 
@@ -19,6 +20,19 @@ public class InsertSort {
     }
 
     private  void insertSort(){
+
+        for(int i=1;i<array.length;i++){
+            int temp = array[i];
+            if(array[i]<array[i-1]){//需要插入了
+                int j =i-1;
+                for(;j>0&&array[j]>temp;j--){ //前面的数据 记录后移
+                    array[j+1] = array[j];
+                }
+                //插入合适的位置
+
+                array[j+1] = temp;
+            }
+        }
 
     }
 
